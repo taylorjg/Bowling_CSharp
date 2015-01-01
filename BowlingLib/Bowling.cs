30d7283e05e5308832aeafbb2b90b7e866c35e71
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-01 22:46:12 +0000

    Factored out a couple of methods

--- BowlingLib/Bowling.cs
+++ BowlingLib/Bowling.cs
@@ -5,6 +5,9 @@ using MonadLib;
 
 namespace BowlingLib
 {
+    using OuterAccumulator = Tuple<IEnumerable<Frame>>;
+    using InnerAccumulator = Tuple<IEnumerable<Frame>, Maybe<int>, bool>;
+
     public static class Bowling
     {
         public const int NumFrames = 10;
@@ -13,37 +16,40 @@ namespace BowlingLib
         public static IEnumerable<Frame> ProcessRolls(IEnumerable<int> rolls)
         {
             var initialFrames = Enumerable.Range(1, NumFrames).Select(frameNumber => new InitialFrame(frameNumber) as Frame);
-
-            var seed1 = Tuple.Create(initialFrames);
-            var aggregateResult = rolls.Aggregate(seed1, (accumulator1, roll) =>
-                {
-                    var currentFrames = accumulator1.Item1;
-
-                    var seed2 = Tuple.Create(Enumerable.Empty<Frame>(), Maybe.Just(0), false);
-                    var aggregateResult2 = currentFrames.Aggregate(seed2, (accumulator2, oldFrame) =>
-                        {
-                            var newFrames = accumulator2.Item1;
-                            var runningTotal = accumulator2.Item2;
-                            var rollWasConsumed = accumulator2.Item3;
-
-                            if (rollWasConsumed)
-                            {
-                                return Tuple.Create(newFrames.Append(oldFrame), Maybe.Nothing<int>(), true);
-                            }
-
-                            var newFrame = oldFrame.ApplyRoll(roll, runningTotal);
-                            var newRunningTotal = newFrame.RunningTotal;
-                            var newRollWasConsumed = oldFrame.WillConsumeRoll;
-
-                            return Tuple.Create(newFrames.Append(newFrame), newRunningTotal, newRollWasConsumed);
-                        });
-
-                    var latestFrames = aggregateResult2.Item1;
-                    return Tuple.Create(latestFrames);
-                });
-
-            var resultantFrames = aggregateResult.Item1;
+            var outerSeed = Tuple.Create(initialFrames);
+            var outerAggregateResult = rolls.Aggregate(outerSeed, ApplyRollToFrames);
+            var resultantFrames = outerAggregateResult.Item1;
             return resultantFrames;
+        }
+
+        private static OuterAccumulator ApplyRollToFrames(OuterAccumulator outerAccumulator, int roll)
+        {
+            Func<InnerAccumulator, Frame, InnerAccumulator> partiallyAppliedApplyRollToFrame =
+                (innerAccumulator, frame) => ApplyRollToFrame(roll, innerAccumulator, frame);
+
+            var frames = outerAccumulator.Item1;
+            var innerSeed = Tuple.Create(Enumerable.Empty<Frame>(), Maybe.Just(0), false);
+            var innerAggregateResult = frames.Aggregate(innerSeed, partiallyAppliedApplyRollToFrame);
+            var newFrames = innerAggregateResult.Item1;
+            return Tuple.Create(newFrames);
+        }
+
+        private static InnerAccumulator ApplyRollToFrame(int roll, InnerAccumulator innerAccumulator, Frame oldFrame)
+        {
+            var newFrames = innerAccumulator.Item1;
+            var runningTotal = innerAccumulator.Item2;
+            var rollWasConsumed = innerAccumulator.Item3;
+
+            if (rollWasConsumed)
+            {
+                return Tuple.Create(newFrames.Append(oldFrame), Maybe.Nothing<int>(), true);
+            }
+
+            var newFrame = oldFrame.ApplyRoll(roll, runningTotal);
+            var newRunningTotal = newFrame.RunningTotal;
+            var newRollWasConsumed = oldFrame.WillConsumeRoll;
+
+            return Tuple.Create(newFrames.Append(newFrame), newRunningTotal, newRollWasConsumed);
         }
     }
 }
