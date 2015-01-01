6372b631a89304066869ff08fb5f20ac9d7828a0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-01 14:38:58 +0000

    Got the basics working

--- BowlingLib/Bowling.cs
+++ BowlingLib/Bowling.cs
@@ -1,19 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BowlingLib
 {
     public static class Bowling
     {
-        public static IEnumerable<Frame> ProcessRolls(IEnumerable<int> rolls)
+        private class Accumulator
         {
-            var seed = Enumerable.Range(1, 10).Select(frameNumber => new InitialFrame(frameNumber) as Frame);
-            return rolls.Aggregate(seed, (accumulator, roll) => accumulator.Select(frame => ProcessRoll(frame, roll)));
+            public Accumulator(IEnumerable<Frame> frames, bool rollWasConsumed)
+            {
+                _frames = frames;
+                _rollWasConsumed = rollWasConsumed;
+            }
+
+            public IEnumerable<Frame> Frames
+            {
+                get { return _frames; }
+            }
+
+            public bool RollWasConsumed
+            {
+                get { return _rollWasConsumed; }
+            }
+
+            private readonly IEnumerable<Frame> _frames;
+            private readonly bool _rollWasConsumed;
         }
 
-        private static Frame ProcessRoll(Frame frame, int roll)
+        public static IEnumerable<Frame> ProcessRolls(IEnumerable<int> rolls)
         {
-            return frame.ApplyRoll(roll);
+            var initialFrames = Enumerable.Range(1, 10).Select(frameNumber => new InitialFrame(frameNumber) as Frame);
+            var seed = new Accumulator(initialFrames, false);
+            var aggregateResult = rolls.Aggregate(seed, (accumulator, roll) =>
+                {
+                    var rollWasConsumed = false;
+                    var newFrames = accumulator.Frames.Select(frame =>
+                        {
+                            if (rollWasConsumed) return frame;
+                            var processRollResult = ProcessRoll(frame, roll);
+                            rollWasConsumed = processRollResult.Item2;
+                            return processRollResult.Item1;
+                        });
+                    return new Accumulator(newFrames, rollWasConsumed);
+                });
+            return aggregateResult.Frames;
+        }
+
+        private static Tuple<Frame, bool> ProcessRoll(Frame frame, int roll)
+        {
+            var rollWasConsumed = frame.WillConsumeRoll;
+            var newFrame = frame.ApplyRoll(roll);
+            return Tuple.Create(newFrame, rollWasConsumed);
         }
     }
 }
