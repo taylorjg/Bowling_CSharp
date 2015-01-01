eca918889199bb0752578f5299658b3fd5eaa61e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-01 14:45:21 +0000

    A few little refactorings

--- BowlingLib/Bowling.cs
+++ BowlingLib/Bowling.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace BowlingLib
 {
     public static class Bowling
     {
-        private class Accumulator
+        private class ProcessRollResult
         {
-            public Accumulator(IEnumerable<Frame> frames, bool rollWasConsumed)
+            public ProcessRollResult(Frame frame, bool rollWasConsumed)
             {
-                _frames = frames;
+                _frame = frame;
                 _rollWasConsumed = rollWasConsumed;
             }
 
-            public IEnumerable<Frame> Frames
+            public Frame Frame
             {
-                get { return _frames; }
+                get { return _frame; }
             }
 
             public bool RollWasConsumed
@@ -24,34 +23,29 @@ namespace BowlingLib
                 get { return _rollWasConsumed; }
             }
 
-            private readonly IEnumerable<Frame> _frames;
+            private readonly Frame _frame;
             private readonly bool _rollWasConsumed;
         }
 
         public static IEnumerable<Frame> ProcessRolls(IEnumerable<int> rolls)
         {
-            var initialFrames = Enumerable.Range(1, 10).Select(frameNumber => new InitialFrame(frameNumber) as Frame);
-            var seed = new Accumulator(initialFrames, false);
-            var aggregateResult = rolls.Aggregate(seed, (accumulator, roll) =>
+            var seed = Enumerable.Range(1, 10).Select(frameNumber => new InitialFrame(frameNumber) as Frame);
+            return rolls.Aggregate(seed, (accumulator, roll) =>
                 {
                     var rollWasConsumed = false;
-                    var newFrames = accumulator.Frames.Select(frame =>
+                    return accumulator.Select(frame =>
                         {
                             if (rollWasConsumed) return frame;
                             var processRollResult = ProcessRoll(frame, roll);
-                            rollWasConsumed = processRollResult.Item2;
-                            return processRollResult.Item1;
+                            rollWasConsumed = processRollResult.RollWasConsumed;
+                            return processRollResult.Frame;
                         });
-                    return new Accumulator(newFrames, rollWasConsumed);
                 });
-            return aggregateResult.Frames;
         }
 
-        private static Tuple<Frame, bool> ProcessRoll(Frame frame, int roll)
+        private static ProcessRollResult ProcessRoll(Frame frame, int roll)
         {
-            var rollWasConsumed = frame.WillConsumeRoll;
-            var newFrame = frame.ApplyRoll(roll);
-            return Tuple.Create(newFrame, rollWasConsumed);
+            return new ProcessRollResult(frame.ApplyRoll(roll), frame.WillConsumeRoll);
         }
     }
 }
--- UnitTests/BowlingTests.cs
+++ UnitTests/BowlingTests.cs
@@ -10,26 +10,8 @@ namespace UnitTests
     {
         private static readonly Maybe<int> NothingRoll = Maybe.Nothing<int>();
         
-        // [Test]
-        // public void Test1()
-        // {
-        //     var actual = Bowling.ProcessRolls(new[] {10, 7, 3, 7, 2, 9, 1, 10, 10, 10, 2, 3, 6, 4, 7, 3, 3});
-        //     Assert.That(actual.IsRight, Is.True);
-        //     var frames = actual.Right.ToArray();
-        //     AssertFrame(frames[0], 1, Maybe.Just(20), Maybe.Just(10), NothingRoll);
-        //     AssertFrame(frames[1], 2, Maybe.Just(37), Maybe.Just(7), Maybe.Just(3));
-        //     AssertFrame(frames[2], 3, Maybe.Just(46), Maybe.Just(7), Maybe.Just(2));
-        //     AssertFrame(frames[3], 4, Maybe.Just(66), Maybe.Just(9), Maybe.Just(1));
-        //     AssertFrame(frames[4], 5, Maybe.Just(96), Maybe.Just(10), NothingRoll);
-        //     AssertFrame(frames[5], 6, Maybe.Just(118), Maybe.Just(10), NothingRoll);
-        //     AssertFrame(frames[6], 7, Maybe.Just(133), Maybe.Just(10), NothingRoll);
-        //     AssertFrame(frames[7], 8, Maybe.Just(138), Maybe.Just(2), Maybe.Just(3));
-        //     AssertFrame(frames[8], 9, Maybe.Just(155), Maybe.Just(6), Maybe.Just(4));
-        //     AssertFrame(frames[9], 10, Maybe.Just(168), Maybe.Just(7), Maybe.Just(3), Maybe.Just(3));
-        // }
-
         [Test]
-        public void Test2()
+        public void Test1()
         {
             var actual = Bowling.ProcessRolls(new[] {10, 7, 3, 7, 2, 9, 1, 10, 10, 10, 2, 3, 6, 4, 7, 3, 3});
             var frames = actual.ToArray();
