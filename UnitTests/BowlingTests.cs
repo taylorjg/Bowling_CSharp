bbf01c23b2ef9c59d3766721c12de09383273bee
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-01 16:20:07 +0000

    Added another unit test re 12 x 10 = 300

--- UnitTests/BowlingTests.cs
+++ UnitTests/BowlingTests.cs
@@ -27,6 +27,23 @@ namespace UnitTests
             AssertFrame(frames[9], 10, Maybe.Just(168), Maybe.Just(7), Maybe.Just(3), Maybe.Just(3));
         }
 
+        [Test]
+        public void Test2()
+        {
+            var actual = Bowling.ProcessRolls(Enumerable.Repeat(10, 12));
+            var frames = actual.ToArray();
+            AssertFrame(frames[0], 1, Maybe.Just(30), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[1], 2, Maybe.Just(60), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[2], 3, Maybe.Just(90), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[3], 4, Maybe.Just(120), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[4], 5, Maybe.Just(150), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[5], 6, Maybe.Just(180), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[6], 7, Maybe.Just(210), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[7], 8, Maybe.Just(240), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[8], 9, Maybe.Just(270), Maybe.Just(10), NothingRoll);
+            AssertFrame(frames[9], 10, Maybe.Just(300), Maybe.Just(10), Maybe.Just(10), Maybe.Just(10));
+        }
+
         private static void AssertFrame(
             Frame frame,
             int frameNumber,
