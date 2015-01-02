789e7e8dc2bf2965d3b856fc4dab1e26217fac43
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-02 13:13:46 +0000

    Implemented first property test in F#

--- PropertyTestsFs/BowlingTests.fs
+++ PropertyTestsFs/BowlingTests.fs
@@ -7,6 +7,16 @@ open FsCheck.Fluent
 open System.Linq
 open BowlingLib
 
+type MyArbitraries =
+  static member NonShrinkingIntArray() =
+      { new Arbitrary<int[]>() with
+          override x.Generator = Gen.constant [||]
+          override x.Shrinker _ = Seq.empty }    
+
+[<SetUp>]
+let setUp =
+    DefaultArbitraries.Add<MyArbitraries>() |> ignore
+
 let nonStrikeValidFrameRolls = 
     seq {
         for r1 in 0..9 do
@@ -34,5 +44,28 @@ let genRolls (genFrame:Gen<int list>) =
         let rollsForTenFrames = Seq.concat tenFrames
         let bonusBalls = Seq.concat bonusBallFrames
         let numBonusBallsNeeded = calculateNumBonusBallsNeeded rollsForlastFrame
-        return [rollsForTenFrames; Seq.take numBonusBallsNeeded bonusBalls] |> Seq.concat
+        return [rollsForTenFrames; Seq.take numBonusBallsNeeded bonusBalls] |> Seq.concat |> Seq.toArray
     }
+
+let checkFrameInvariant (f:Frame) : bool =
+    f.RunningTotal.IsJust &&
+    f.FirstRoll.IsJust &&
+    if f.IsLastFrame then
+        f.SecondRoll.IsJust &&
+        if f.IsStrikeFrame || f.IsSpareFrame then f.ThirdRoll.IsJust else f.ThirdRoll.IsNothing
+    else
+        let r1 = f.FirstRoll.FromMaybe 0
+        let r2 = f.SecondRoll.FromMaybe 0
+        r1 + r2 <= 10 &&
+        (if f.IsStrikeFrame then f.SecondRoll.IsNothing else true) &&
+        f.ThirdRoll.IsNothing
+
+let prop_FrameInvariantHoldsForAllFrames rolls : bool =
+    let frames = Bowling.ProcessRolls rolls
+    Seq.forall checkFrameInvariant frames
+
+[<Property>]
+let ``frame invariant holds for all frames``() = 
+    let gen = genRolls genValidFrame
+    let specBuilder = Spec.For (gen, prop_FrameInvariantHoldsForAllFrames)
+    specBuilder.QuickCheckThrowOnFailure()
